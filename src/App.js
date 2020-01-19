a054b182a3ca4eec95dde68f5988ac8855e954e2
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-20 08:19:12 +1000

    Todos now addable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList'
 
 function App(){
-  const [todos, setTodos] = useState([ 'hello' , 'yolo'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function addTodo(e){
+    const name = todoNameRef.current.value
+    if (name == '') return 
+    
+    setTodos(prevTodos => {
+      return [...prevTodos, {id : 1, name: name, complete: false}]
+    })
+
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={addTodo}>Add Todo</button>
       <button>Clear Completed</button>
       <div>0 left to do</div>
     </>
---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Todo({todo}) {
+    return (
+        <div>
+        <label>
+            {todo.name}
+            <input type='checkbox' checked={todo.complete} ></input>
+        </label>
+        </div>
+    )
+}
+
