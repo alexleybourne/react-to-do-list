f614aa75824ae9b2429f195a9f9882ff91302aa3
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-20 08:33:23 +1000

    Saves to local storage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
 
 function App(){
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
+  // Checks for stored todos and then sets them
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // Saves Todos to local Storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  // Creates todo from input
   function addTodo(e){
     const name = todoNameRef.current.value
     if (name == '') return 
     
+    // Values for each object with unique id 
     setTodos(prevTodos => {
-      return [...prevTodos, {id : 1, name: name, complete: false}]
+      return [...prevTodos, {id : uuidv4(), name: name, complete: false}]
     })
+    
 
     todoNameRef.current.value = null
   }
 
+  // Html (JSX)
   return (
     <>
       <TodoList todos={todos} />
